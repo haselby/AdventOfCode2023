61dd0f60792838e64250757c84b851c1843783a0
Haselby <jeffrey.haselby@hp.com>
2023-11-30 22:33:27 -0700

    Added PuzzleInput and PuzzleInputTest classes

--- src/day01/FirstPuzzle.java
+++ src/day01/FirstPuzzle.java
@@ -1,7 +1,11 @@
 package day01;
 
+import java.io.IOException;
+import java.util.ArrayList;
 public class FirstPuzzle {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
         System.out.println("Starting Day 1, Puzzle 1");
+        ArrayList<String> puzzleInput = PuzzleInput.readIntoListOfStrings("input_day01.txt");
     }
 }
---
+++ src/day01/PuzzleInput.java
@@ -0,0 +1,28 @@
+package day01;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+public class PuzzleInput {
+    public static ArrayList<String> readIntoListOfStrings(String filename) throws IOException {
+
+        ArrayList<String> listOfStrings = new ArrayList<>();
+
+        // Find input file in resources folder: /resources
+        InputStream inputStream = PuzzleInput.class.getResourceAsStream("/" + filename);
+        if (inputStream == null) throw new IOException("Input filename does not exist!");
+
+        try (BufferedReader puzzleInputFile = new BufferedReader(new InputStreamReader(inputStream))) {
+            String input;
+            while ((input = puzzleInputFile.readLine()) != null) {
+                listOfStrings.add(input);
+            }
+
+        } catch (IOException e) {
+            throw new IOException();
+        }
+
+        return listOfStrings;
+    }
+}
