fd0ed8a8462f0ee702eb702249f893b5bd529a8e
Haselby <jeffrey.haselby@hp.com>
2023-12-02 10:57:39 -0700

    Added RGBCubes and RGBCubesTest classes

---
+++ test/day02/RGBCubesTest.java
@@ -0,0 +1,33 @@
+package day02;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RGBCubesTest {
+    private RGBCubes rgbCubes;
+
+    @BeforeEach
+    public void setUp() {
+        rgbCubes = new RGBCubes(255, 255, 255);
+    }
+
+    @Test
+    public void testRed() {
+        rgbCubes.setRed(128);
+        assertEquals(128, rgbCubes.getRed());
+    }
+
+    @Test
+    public void testGreen() {
+        rgbCubes.setGreen(128);
+        assertEquals(128, rgbCubes.getGreen());
+    }
+
+    @Test
+    public void testBlue() {
+        rgbCubes.setBlue(128);
+        assertEquals(128, rgbCubes.getBlue());
+    }
+}
+
