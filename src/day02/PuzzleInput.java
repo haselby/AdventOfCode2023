7757bcb613f4734249976bfc9827c3325ff35db9
Haselby <jeffrey.haselby@hp.com>
2023-12-03 17:00:01 -0700

    Created Day 02 PuzzleInput and PuzzleInputTest class capable of loading input into list of CubeGames

--- src/day02/PuzzleInput.java
+++ src/day02/PuzzleInput.java
@@ -1,9 +1,11 @@
 package day02;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
 public class PuzzleInput {
     public static ArrayList<String> readIntoListOfStrings(String filename) throws IOException {
@@ -26,4 +28,50 @@ public class PuzzleInput {
 
         return listOfStrings;
     }
+
+    public static ArrayList<CubeGame> readIntoListOfCubeGames(String filename) throws IOException {
+
+        ArrayList<CubeGame> listOfCubeGames = new ArrayList<>();
+        ArrayList<String> listOfLines = day01.PuzzleInput.readIntoListOfStrings(filename);
+
+        for (String line : listOfLines) {
+            String[] parts = line.split(": ");
+            Integer gameId = Integer.parseInt(parts[0].split(" ")[1]);
+            String[] cubeDescriptions = parts[1].split("; ");
+            List<RGBCubes> cubesList = new ArrayList<>();
+
+            for (String cubeDescription : cubeDescriptions) {
+                String[] colorCounts = cubeDescription.split(", ");
+                RGBCubes rgbCubes = new RGBCubes(0, 0, 0);
+
+                for (String colorCount : colorCounts) {
+                    String[] countAndColor = colorCount.split(" ");
+                    Integer count = Integer.parseInt(countAndColor[0]);
+                    String color = countAndColor[1];
+
+                    switch (color) {
+                        case "red":
+                            rgbCubes.setRed(count);
+                            break;
+                        case "green":
+                            rgbCubes.setGreen(count);
+                            break;
+                        case "blue":
+                            rgbCubes.setBlue(count);
+                            break;
+                    }
+                }
+
+                cubesList.add(rgbCubes);
+            }
+
+            CubeGame cubeGame = new CubeGame(gameId, cubesList);
+            listOfCubeGames.add(cubeGame);
+        }
+
+
+        return listOfCubeGames;
+    }
+
+
 }
--- test/day02/PuzzleInputTest.java
+++ test/day02/PuzzleInputTest.java
@@ -38,7 +38,7 @@ public class PuzzleInputTest {
     @Test
     @DisplayName("readIntoListOfStrings - passing example input file - should not throw exception")
     void readIntoListOfStrings_ExampleInputFile_ShouldNotThrowException() {
-        assertDoesNotThrow(() -> day01.PuzzleInput.readIntoListOfStrings("input_day01-01.txt"));
+        assertDoesNotThrow(() -> day01.PuzzleInput.readIntoListOfStrings("input_day02-01.txt"));
     }
 
     @Test
@@ -54,17 +54,95 @@ public class PuzzleInputTest {
         List<String> puzzleInputTest01 = new ArrayList<>();
 
         try {
-            puzzleInputTest01 = PuzzleInput.readIntoListOfStrings("input_day01-01.txt");
+            puzzleInputTest01 = PuzzleInput.readIntoListOfStrings("input_day02-01.txt");
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        assertEquals("1abc2", puzzleInputTest01.get(0));
-        assertEquals("pqr3stu8vwx", puzzleInputTest01.get(1));
-        assertEquals("a1b2c3d4e5f", puzzleInputTest01.get(2));
-        assertEquals("treb7uchet", puzzleInputTest01.get(3));
+        assertEquals("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", puzzleInputTest01.get(0));
+        assertEquals("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", puzzleInputTest01.get(1));
+        assertEquals("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", puzzleInputTest01.get(2));
+        assertEquals("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", puzzleInputTest01.get(3));
+        assertEquals("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", puzzleInputTest01.get(4));
     }
+
+    @Test
+    @DisplayName("readIntoListOfCubeGames - passing example input file - should not throw exception")
+    void readIntoListOfCubeGames_ExampleInputFile_ShouldNotThrowException() {
+        assertDoesNotThrow(() -> day02.PuzzleInput.readIntoListOfCubeGames("input_day02-01.txt"));
+    }
+
+    @Test
+    @DisplayName("readIntoListOfCubeGames - passing example input file - returns expected gameIDs")
+    void readIntoListOfCubeGames_ExampleInputFile_ReturnsExpectedGameIDs() {
+
+        // Call readIntoListOfCubeGames method
+        List<CubeGame> returnedCubeGames = new ArrayList<>();
+        try {
+            returnedCubeGames = day02.PuzzleInput.readIntoListOfCubeGames("input_day02-01.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Compare gameIDs
+        assertEquals(1, returnedCubeGames.get(0).getGameId());
+        assertEquals(2, returnedCubeGames.get(1).getGameId());
+        assertEquals(3, returnedCubeGames.get(2).getGameId());
+        assertEquals(4, returnedCubeGames.get(3).getGameId());
+        assertEquals(5, returnedCubeGames.get(4).getGameId());
+
+    }
+
+    @Test
+    @DisplayName("readIntoListOfCubeGames - passing example input file - returns expected number of RGBCubes sets")
+    void readIntoListOfCubeGames_ExampleInputFile_ReturnsExpectedNumberOfRGBCubesSets() {
+
+        // Call readIntoListOfCubeGames method
+        List<CubeGame> returnedCubeGames = new ArrayList<>();
+        try {
+            returnedCubeGames = day02.PuzzleInput.readIntoListOfCubeGames("input_day02-01.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Compare gameIDs
+        assertEquals(3, returnedCubeGames.get(0).getCubes().size());
+        assertEquals(3, returnedCubeGames.get(1).getCubes().size());
+        assertEquals(3, returnedCubeGames.get(2).getCubes().size());
+        assertEquals(3, returnedCubeGames.get(3).getCubes().size());
+        assertEquals(2, returnedCubeGames.get(4).getCubes().size());
+
+    }
+
+    @Test
+    @DisplayName("readIntoListOfCubeGames - passing example input file - returns expected number of RGBCubes sets")
+    void readIntoListOfCubeGames_ExampleInputFile_ReturnsExpectedNumberOfRGBCubesInASpecificSet() {
+
+        // Call readIntoListOfCubeGames method
+        List<CubeGame> returnedCubeGames = new ArrayList<>();
+        try {
+            returnedCubeGames = day02.PuzzleInput.readIntoListOfCubeGames("input_day02-01.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Compare cube colors in first game, in first set to expected values
+        assertEquals(4, returnedCubeGames.get(0).getCubes().get(0).getRed());
+        assertEquals(0, returnedCubeGames.get(0).getCubes().get(0).getGreen());
+        assertEquals(3, returnedCubeGames.get(0).getCubes().get(0).getBlue());
+
+        // Compare cube colors in first game, in second set to expected values
+        assertEquals(1, returnedCubeGames.get(0).getCubes().get(1).getRed());
+        assertEquals(2, returnedCubeGames.get(0).getCubes().get(1).getGreen());
+        assertEquals(6, returnedCubeGames.get(0).getCubes().get(1).getBlue());
+
+        // Compare cube colors in second game, in first set to expected values
+        assertEquals(0, returnedCubeGames.get(1).getCubes().get(0).getRed());
+        assertEquals(2, returnedCubeGames.get(1).getCubes().get(0).getGreen());
+        assertEquals(1, returnedCubeGames.get(1).getCubes().get(0).getBlue());
+
+
+
+    }
+
 
 }
 
